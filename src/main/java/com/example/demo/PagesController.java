7c104344f919b229883edf5205b595f5ac2271ff
daskalyan1 <kalyan_115@yahoo.com>
2018-07-29 01:11:26 -0600

    Unit3 exercise1

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,15 +1,22 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
+@RequestMapping("/math")
 public class PagesController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/pi")
+    public double getPi() {
+        return Math.PI;
     }
 }
 
